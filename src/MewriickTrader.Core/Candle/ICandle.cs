aae34ce44a619aaa0b9ef067b602102fda46b4b5
Jaroslav Surala <SuralaJ@gmail.com>
2017-11-18 12:01:19 +0100

    Initial project structure

---
+++ src/MewriickTrader.Core/Candle/ICandle.cs
@@ -0,0 +1,13 @@
+﻿namespace MewriickTrader.Core.Candle
+{
+    public interface ICandle : ITick
+    {
+        decimal Open { get; }
+
+        decimal Close { get; }
+
+        decimal High { get; }
+
+        decimal Low { get; }
+    }
+}
---
+++ src/MewriickTrader.Core/Candle/ITick.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MewriickTrader.Core.Candle
+{
+    public interface ITick
+    {
+        DateTimeOffset DateTime { get; }
+    }
+}
