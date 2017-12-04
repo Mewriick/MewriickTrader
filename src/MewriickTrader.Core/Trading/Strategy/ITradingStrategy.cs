e53977987883b075145fd12e6874eaebe20b4199
Jaroslav Surala <SuralaJ@gmail.com>
2017-12-04 21:09:51 +0100

    Modify TradingStrategy

--- src/MewriickTrader.Core/Trading/Strategy/ITradingStrategy.cs
+++ src/MewriickTrader.Core/Trading/Strategy/ITradingStrategy.cs
@@ -1,4 +1,5 @@
-﻿using MewriickTrader.Core.Trading.Market;
+﻿using MewriickTrader.Core.Candle;
+using MewriickTrader.Core.Trading.Market;
 using System;
 
 namespace MewriickTrader.Core.Trading.Strategy
@@ -7,9 +8,15 @@ namespace MewriickTrader.Core.Trading.Strategy
     {
         /// <summary>
         /// Called on each bar update event (incoming tick)
+        /// Strategy exit rules (for quickier possibility to react)
+        /// </summary>
+        void OnTimeBarUpdate(IQuote quote);
+
+        /// <summary>
+        /// Called when time bar is closed
         /// Strategy enter/exit/filtering rules
         /// </summary>
-        void OnTimeBarUpdate(IQuote quoteInfo);
+        void OnTimeBarClosed(ICandle timeBar);
 
         void StartListeningMarket();
 
--- src/MewriickTrader.Core/Trading/Strategy/TradingStrategy.cs
+++ src/MewriickTrader.Core/Trading/Strategy/TradingStrategy.cs
@@ -1,4 +1,6 @@
-﻿using MewriickTrader.Core.Trading.Market;
+﻿using MewriickTrader.Core.Candle;
+using MewriickTrader.Core.Trading.Charts;
+using MewriickTrader.Core.Trading.Market;
 using System;
 
 namespace MewriickTrader.Core.Trading.Strategy
@@ -6,28 +8,46 @@ namespace MewriickTrader.Core.Trading.Strategy
     public abstract class TradingStrategy : ITradingStrategy
     {
         private bool timeBarUpdatedSubscriptionDisposed;
+        private bool timeBarClosedSubscriptionDisposed;
         private bool disposed = false;
 
         private IDisposable timeBarUpdatedSubscription;
+        private IDisposable timeBarClosedSubscription;
 
+        protected IMarketTimerBars timeBarsListener;
         protected IMarketListener marketListener;
 
+        public bool SubscribeOnlyOnBarClose { get; set; } = true;
 
-        public TradingStrategy(IMarketListener marketListener)
+
+        public TradingStrategy(IMarketTimerBars timeBarsListener, IMarketListener marketListener)
         {
-            this.marketListener = marketListener;
+            this.timeBarsListener = timeBarsListener ?? throw new ArgumentNullException(nameof(timeBarsListener));
+            this.marketListener = marketListener ?? throw new ArgumentNullException(nameof(marketListener));
         }
 
         public void StartListeningMarket()
         {
-            timeBarUpdatedSubscription = marketListener.TimeBarUpdated.Subscribe(OnTimeBarUpdate);
+            timeBarUpdatedSubscription = timeBarsListener.TimeBarAdded.Subscribe(OnTimeBarClosed);
             timeBarUpdatedSubscriptionDisposed = false;
+
+            if (!SubscribeOnlyOnBarClose)
+            {
+                timeBarClosedSubscription = marketListener.TimeBarUpdated.Subscribe(OnTimeBarUpdate);
+                timeBarClosedSubscriptionDisposed = false;
+            }
         }
 
         public void StopListeningMarket()
         {
             timeBarUpdatedSubscription.Dispose();
             timeBarUpdatedSubscriptionDisposed = true;
+
+            if (timeBarClosedSubscription != null)
+            {
+                timeBarClosedSubscription.Dispose();
+                timeBarClosedSubscriptionDisposed = true;
+            }
         }
 
         public void Dispose()
@@ -46,6 +66,11 @@ namespace MewriickTrader.Core.Trading.Strategy
                 {
                     timeBarUpdatedSubscription.Dispose();
                 }
+
+                if (!timeBarClosedSubscriptionDisposed)
+                {
+                    timeBarClosedSubscription?.Dispose();
+                }
             }
 
             disposed = true;
@@ -53,8 +78,19 @@ namespace MewriickTrader.Core.Trading.Strategy
 
         /// <summary>
         /// Called on each bar update event (incoming tick)
-        /// Suited for strategy enter/exit/filtering rules
+        /// Strategy exit rules (for quickier possibility to react)
         /// </summary>
-        public abstract void OnTimeBarUpdate(IQuote quoteInfo);
+        public virtual void OnTimeBarUpdate(IQuote quote)
+        {
+            throw new NotImplementedException($"You set property {nameof(SubscribeOnlyOnBarClose)} to True, you must override method {nameof(OnTimeBarUpdate)}" +
+                $" for handling every market tick");
+        }
+
+        /// <summary>
+        /// Called when time bar is closed
+        /// Strategy enter/exit/filtering rules
+        /// </summary>
+        public abstract void OnTimeBarClosed(ICandle timeBar);
+
     }
 }
